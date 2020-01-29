72e18f96f77d61095b6666142d558b3a02afe67e
Seyon Rajagopal <r_seyon@live.ca>
2020-01-28 22:25:29 -0500

    Update Repos to automatically fetch

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var menu = document.getElementById("mobile-menu");
-
+var user = "seyon123";
+window.onload = genRepo(user);
 
 // Code for typist effect
 var TxtType = function (el, toRotate, period) {
@@ -59,6 +60,59 @@ window.onload = function () {
     document.body.appendChild(css);
 };
 
+
+
+function genRepo(user) {
+    const testuser = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
+
+    if (testuser.test(user) == false || user == "" || user == null) {
+        $("#repo-box").append("<div class='error-box'><h1 class='error-msg'> Sorry the GitHub username appears to be invalid </h1></div>");
+    }
+
+    else {
+
+        var requestURL = 'https://api.github.com/users/' + user + '/repos';
+        var request = $.get(requestURL, function () {
+        })
+            .done(function () {
+                request = request.responseJSON;
+                if (!Array.isArray(request) || !request.length) {
+                    $("#repo-box").append("<div class='error-box'><h1 class='error-msg'> Sorry the GitHub username entered has no repos or does't exist </h1></div>");
+                }
+                else {
+                    for (i = 0; i < request.length; i++) {
+                        // variables from api request
+                        var repo_url = request[i].html_url;
+                        var username = request[i].owner.login;
+                        var repo_name = request[i].name;
+                        var repo_description = request[i].description;
+                        var repo_language = request[i].language;
+                        var repo_stars = request[i].stargazers_count;
+                        var repo_forks = request[i].forks;
+
+                        // replaces null values to be better represented when displayed
+                        if (repo_description == null) {
+                            repo_description = "<i>No Description</i>";
+                        }
+                        if (repo_language == null) {
+                            repo_language = "-";
+                        }
+
+                        // Puts repo information into div
+                        $("#repo-box").append("<a href='" + repo_url + "' target='_blank'><div class='repo-item'><h1 class='title'>" +
+                            username + "/" +
+                            repo_name + "</h1><p class='description'>" +
+                            repo_description + "</p>" + "<div class='bottom'><div class='language'><i class='img fa fa-code'></i>" +
+                            repo_language + "</div>  <div class='star'><i class='img fa fa-star'></i>" +
+                            repo_stars + "  </div> <div class='fork'><i class='img fa fa-code-fork'></i>" +
+                            repo_forks + "</div></div></div>");
+                    }
+                }
+            });
+    }
+}
+
+
 // code for side menu
 function openNav() {
     document.getElementById("side-menu").style.height = "100%";
@@ -69,3 +123,4 @@ function closeNav() {
     document.getElementById("side-menu").style.height = "0";
     document.getElementById("side-menu").style.paddingTop = "0px";
 }
+
